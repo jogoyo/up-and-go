01d64e22e1249662befff6e3e7ba65e50216d84b
Joseph Di Pasquale <joseph@josephdp.com>
2017-06-18 20:57:37 -0400

    Partially finished app. Renamed ViewController, only missing ability to delete tasks.

---
+++ Up and GO/CreateTaskViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateTaskViewController.swift
+//  Up and GO
+//
+//  Created by Joseph Di Pasquale on 2017-06-18.
+//  Copyright © 2017 SiteWiz. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    @IBAction func addTapped(_ sender: Any) {
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+    }
+
+}
---
+++ Up and GO/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Up and GO
+//
+//  Created by Joseph Di Pasquale on 2017-06-18.
+//  Copyright © 2017 SiteWiz. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
